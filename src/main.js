28f86a7dd63915ed1d4a0cce49a9c4d3b9ef81c6
Antério Vieira da Silva <anteriovieira@gmail.com>
2017-06-07 12:26:49 -0300

    Update pages

--- src/main.js
+++ src/main.js
@@ -9,11 +9,13 @@ import ngLocale from 'angular-i18n/pt-br'
 
 import app from './App'
 import store from './store'
+
+import homePage  from './modules/home/HomePage'
 import fleetPage from './modules/fleet/FleetPage'
 import aboutPage from './modules/about/AboutPage'
 
 angular.module('app', [uiRouter, ngLocale, store])
-.components( { app, fleetPage, aboutPage })
+.components( { app, homePage, fleetPage, aboutPage })
 
 .directive('caPopover', function () {
     return {
@@ -24,14 +26,28 @@ angular.module('app', [uiRouter, ngLocale, store])
     }
 })
 
+.config(['$locationProvider', '$urlRouterProvider', ($locationProvider, $urlRouterProvider) => {
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
+
+    // if the path doesn't match any of the urls you configured
+    // otherwise will take care of routing the user to the specified url
+    $urlRouterProvider.otherwise('/home');
+}])
+
 .config(['$stateProvider', ({ state }) => {
 
-    state('fleet', {
-        url: '',
-        component: 'fleetPage'
+    state('home', {
+        url: '/home',
+        component: 'homePage'
     })
 
-    store('about', {
+    state('fleet', {
+        url: '/fleet',
+        component: 'fleetPage'
+    })
+    
+    state('about', {
         url: '/about',
         component: 'aboutPage'
     })
